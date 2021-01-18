98dfada85d267e5493c9843f452cdc7ba55ce5ff
Ekaterina_Nikonova <ekaterina_nikonova@epam.com>
2021-01-18 22:18:47 +0300

    HW4 updates to test Jenkins job run

--- src/test/java/hw3/data/AbstractPageTest.java
+++ src/test/java/hw3/data/AbstractPageTest.java
@@ -2,6 +2,7 @@ package hw3.data;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.ITestContext;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
@@ -11,9 +12,10 @@ public class AbstractPageTest {
     protected WebDriver driver;
 
     @BeforeClass(alwaysRun = true)
-    public void ChromeSetUp() {
+    public void ChromeSetUp(ITestContext testContext) {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
+        testContext.setAttribute("driver", driver);
     }
 
 
